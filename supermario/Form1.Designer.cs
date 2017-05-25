f3d80cdd7b5eeddf6b1dff7d557e8b3df4a60524
Darek <darek95asz.gmail.com>
2017-05-25 21:18:40 +0200

    blockJumper

---
+++ supermario/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace supermario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.screen = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.block = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.screen.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.block)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // screen
+            // 
+            this.screen.Controls.Add(this.label1);
+            this.screen.Controls.Add(this.block);
+            this.screen.Controls.Add(this.player);
+            this.screen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.screen.Location = new System.Drawing.Point(0, 0);
+            this.screen.Name = "screen";
+            this.screen.Size = new System.Drawing.Size(628, 370);
+            this.screen.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "0";
+            // 
+            // block
+            // 
+            this.block.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.block.Location = new System.Drawing.Point(276, 185);
+            this.block.Name = "block";
+            this.block.Size = new System.Drawing.Size(208, 50);
+            this.block.TabIndex = 1;
+            this.block.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.player.Location = new System.Drawing.Point(119, 113);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(25, 25);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 370);
+            this.Controls.Add(this.screen);
+            this.Name = "Form1";
+            this.Text = "SuperMario";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.screen.ResumeLayout(false);
+            this.screen.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.block)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel screen;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox block;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ supermario/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace supermario
+{
+    public partial class Form1 : Form
+    {
+
+        private bool onBlock = false;
+        private bool right;
+        private bool left;
+        private bool jump=false;
+        private int speed = 30;
+        private int force;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+          
+            
+            
+            //collisions left/right
+            if(player.Right > block.Left && player.Left < block.Right - player.Width/2  && player.Top>block.Top && player.Top<block.Bottom)
+            {
+                right = false;
+            }
+            if (player.Left < block.Right && player.Right > block.Left + player.Width / 2 && player.Bottom > block.Top && player.Top > block.Top && player.Top < block.Bottom)
+            {
+                left = false;
+            }
+            //collision top
+
+            if ((player.Left + player.Width - 5 > block.Left) && (player.Right - player.Width + 5 < block.Right) && (player.Top + player.Height > block.Top) && (player.Top < block.Top))
+            {
+                player.Top = block.Top - player.Height;
+                force = 0;
+                jump = false;
+                onBlock = true;
+            }
+
+            if (player.Left > block.Right || player.Right < block.Left) onBlock= false;
+            
+
+            //collision bottom
+            if ((player.Left + player.Width - 5 > block.Left) && (player.Right - player.Width + 5 < block.Right) && 
+                 (player.Top <= block.Bottom) && player.Bottom>block.Bottom)
+            {
+                player.Top = block.Bottom-1 ;
+                force = 0;
+                jump = false;
+            }
+
+            if (right == true)
+            {
+                player.Left += 5;
+            }
+            if(left == true) { player.Left -= 5; }
+
+            if(jump == true)
+            {
+                
+                if(force >0)player.Top -= force--;
+
+            }
+
+            if(player.Top + player.Height >= screen.Height)
+            {
+                player.Top = screen.Height - player.Height;
+                jump = false;
+            }
+            else
+            {
+                if(onBlock==false)
+                    player.Top += 10;  
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Right) { right = true; }
+            if (e.KeyCode == Keys.Left) { left = true; }
+            if (e.KeyCode == Keys.Escape) { this.Close(); }
+
+            if(jump != true)
+            {
+
+                if(e.KeyCode == Keys.Space)
+                {
+                    onBlock = false;
+                    jump = true;
+                    force = speed;
+                }
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right) { right = false; }
+            if (e.KeyCode == Keys.Left) { left = false; }
+
+        }
+    } 
+}
